9830703800bd1feb4493e4d56fe8f5f2be060d53
Kanishk <itskanishk36@gmail.com>
2026-03-23 21:48:39 +0530

    updated Response.cpp

--- single/tmwp/src/Response.cpp
+++ single/tmwp/src/Response.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<windows.h>
 #include<tmwp>
+#include<string.h>
 #include<utility>
 #include<map>
 using namespace std;
@@ -15,17 +17,19 @@ Response::~Response()
 }
 void Response::setDescriptor(int clientSocketDescriptor)
 {
-
+this->clientSocketDescriptor=clientSocketDescriptor;
 }
 void Response::sendHeader()
 {
-
+char header[8192+1];
+sprintf(header,"HTTP/1.1 200 OK\nContent-Type: text/html\nConnection: close\n\n");
+send(this->clientSocketDescriptor,header,strlen(header),0);
 }
 void Response::write(string html)
 {
-
+send(this->clientSocketDescriptor,html.c_str(),html.size(),0);
 }
 void Response::close()
 {
-
+closesocket(this->clientSocketDescriptor);
 }
