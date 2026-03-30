d35a6abda142e4232dca9cdce1afc31e154726eb
Kanishk <itskanishk36@gmail.com>
2026-03-31 00:27:35 +0530

    added code for forward() method of Request class

--- single/tmwp/src/Request.cpp
+++ single/tmwp/src/Request.cpp
@@ -47,6 +47,9 @@ this->response=response;
 void Request::forward(string requestResource)
 {
 if(requestResource.size()==0) return;
+char header[8193];
+char response[1025];
+char tmp[1001];
 FILE *f;
 int isFile=0;
 for(int i=0;i<requestResource.size();i++)
@@ -58,16 +61,24 @@ if(isFile==1)
 f=fopen(requestResource.c_str(),"rb");
 if(f==NULL)
 {
-//send back 404 error page
+strcpy(response,"<!DOCTYPE HTML>");
+strcat(response,"<html lang='en'>");
+strcat(response,"<head>");
+strcat(response,"<meta charset='utf-8'>");
+strcat(response,"<title>Not Found</title>");
+strcat(response,"</head>");
+strcat(response,"<body>");
+sprintf(tmp,"<h1 style='color: red'>Resource /%s not found</h1>",requestResource.c_str());
+strcat(response,tmp);
+strcat(response,"</body>");
+strcat(response,"</html>");
+send(clientSocketDescriptor,response,strlen(response),0);
 }
 else
 {
 fseek(f,0,SEEK_END);
 int file_size=ftell(f);
 fseek(f,0,SEEK_SET);
-char header[8193];
-char response[1025];
-
 int i=0;
 int toRead;
 while(i<file_size)
@@ -101,18 +112,42 @@ requestResource.erase(0,1);
 map<string,void (*)(Request &,Response &)>::iterator i=server->ptrMap.find(requestResource);
 if(i==server->ptrMap.end())
 {
+strcpy(response,"<!DOCTYPE HTML>");
+strcat(response,"<html lang='en'>");
+strcat(response,"<head>");
+strcat(response,"<meta charset='utf-8'>");
+strcat(response,"<title>Not Found</title>");
+strcat(response,"</head>");
+strcat(response,"<body>");
+sprintf(tmp,"<h1 style='color: red'>Resource /%s not found</h1>",requestResource.c_str());
+strcat(response,tmp);
+strcat(response,"</body>");
+strcat(response,"</html>");
+send(clientSocketDescriptor,response,strlen(response),0);
 closesocket(this->clientSocketDescriptor);
 return;
 }
 void (*ptr)(Request &,Response &)=i->second;
 if(ptr==NULL)
 {
+strcpy(response,"<!DOCTYPE HTML>");
+strcat(response,"<html lang='en'>");
+strcat(response,"<head>");
+strcat(response,"<meta charset='utf-8'>");
+strcat(response,"<title>Not Found</title>");
+strcat(response,"</head>");
+strcat(response,"<body>");
+sprintf(tmp,"<h1 style='color: red'>Resource /%s not found</h1>",requestResource.c_str());
+strcat(response,tmp);
+strcat(response,"</body>");
+strcat(response,"</html>");
+send(clientSocketDescriptor,response,strlen(response),0);
 closesocket(this->clientSocketDescriptor);
 return;
 }
 else
 {
-ptr(*this,*response);
+ptr(*this,*(this->response));
 }
 } // functions
 }
